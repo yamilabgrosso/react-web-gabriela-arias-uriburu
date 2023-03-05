1b5854e98852b8b3aa1020b37dd312f5c4ab4494
Melanie Casasco <melaniecasasco@gmail.com>
2023-03-05 12:47:24 -0300

    fix cards home links

--- src/components/CardItemHome.js
+++ src/components/CardItemHome.js
@@ -9,20 +9,37 @@ function CardItemHome(props) {
     <>
       <li className='cards__item'>
         
+       {
+        props.path.startsWith('/') ? 
         <Link className='cards__item__link' to={props.path} >
-          <figure className='cards__item__pic-wrap'>
-            <img
-              className='cards__item__img'
-              src={props.imagen}
-              alt=""
-            />
-          </figure>
-          <div className='cards__item__info'>
-            <h2 className='cards__item__title'>{props.titulo}</h2>
-            <h5 className='cards__item__text'>{props.texto}</h5>
-            <Button className="btn--primary">{props.textoBoton}</Button>
-          </div>
-        </Link>
+        <figure className='cards__item__pic-wrap'>
+          <img
+            className='cards__item__img'
+            src={props.imagen}
+            alt=""
+          />
+        </figure>
+        <div className='cards__item__info'>
+          <h2 className='cards__item__title'>{props.titulo}</h2>
+          <h5 className='cards__item__text'>{props.texto}</h5>
+          <Button className="btn--primary">{props.textoBoton}</Button>
+        </div>
+      </Link> :
+       <a className='cards__item__link' href={props.path} rel='noreferrer' target='_blank'>
+       <figure className='cards__item__pic-wrap'>
+         <img
+           className='cards__item__img'
+           src={props.imagen}
+           alt=""
+         />
+       </figure>
+       <div className='cards__item__info'>
+         <h2 className='cards__item__title'>{props.titulo}</h2>
+         <h5 className='cards__item__text'>{props.texto}</h5>
+         <Button className="btn--primary">{props.textoBoton}</Button>
+       </div>
+     </a>
+       }
       </li>
     </>
   );
