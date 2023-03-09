13772378a6838a705b3c07dbe3a1cab82aa1a6c2
Melanie Casasco <melaniecasasco@gmail.com>
2023-03-09 08:53:44 -0300

    fix body css

--- src/components/modales-talleres/ModalMadreVida.js
+++ src/components/modales-talleres/ModalMadreVida.js
@@ -9,18 +9,17 @@ function ModalMadreVida() {
       <div>
         <p>Soy hijo ~ Soy Vida <br></br>
 
-Cuando respetamos a mamá tomamos autonomía.<br></br>
+Cuando respetamos a mamá tomamos autonomía.
 
-Más allá de los nombres y las historias todos somos hijos.<br></br>
+Más allá de los nombres y las historias todos somos hijos.
 
-Nuestro primer contacto con la vida viene a través de la madre, nos gestamos dentro, ahí tomamos el alimento, su cuerpo nos da abrigo.<br></br>
+Nuestro primer contacto con la vida viene a través de la madre, nos gestamos dentro, ahí tomamos el alimento, su cuerpo nos da abrigo.
 
-Nuestros corazones durante nueve meses laten juntos, somos dos corazones latientes, ella y yo, yo y ella.<br></br>
+Nuestros corazones durante nueve meses laten juntos, somos dos corazones latientes, ella y yo, yo y ella.
 
-Somos un tambor doble vibrando al unísono.<br></br>
+Somos un tambor doble vibrando al unísono.
 
-Ahora bien, muchos no vivimos esta experiencia del alimento el sustento, la educación, el amor, la caricia … y poco a poco fuimos distanciándonos, ocultando, acorazando, desarrollando patrones, protegiéndonos para seguir adelante , a veces,  hasta nos convertimos en padres de nuestra Madre, sintiéndonos más grandes que ella, sintiéndonos responsables de su bienestar, provocando un desorden en el sistema, y haciendo que mi vida esté disminuida.<br></br>
-
+Ahora bien, muchos no vivimos esta experiencia del alimento el sustento, la educación, el amor, la caricia … y poco a poco fuimos distanciándonos, ocultando, acorazando, desarrollando patrones, protegiéndonos para seguir adelante , a veces,  hasta nos convertimos en padres de nuestra Madre, sintiéndonos más grandes que ella, sintiéndonos responsables de su bienestar, provocando un desorden en el sistema, y haciendo que mi vida esté disminuida.
 Recobrar mi lugar, respetar y aceptar a mamá exactamente como es, es el desafío que te proponemos en este taller.<br></br>
 
 ¿Estás dispuesta a tomar la Vida?
